22aecb10d7affecdbdfedb84d4484c0fe0978612
Houwang Tu <tuhouwang96@163.com>
2020-12-07 22:29:45 +0800

    更新格式

--- src/ChebTransFFT.m
+++ src/ChebTransFFT.m
@@ -11,7 +11,7 @@ function fk = ChebTransFFT(N, fx)
 %  fk:          Chebyshev coefficients of fx
 %--------------------------------------------------------------------------
 
-    fk = fft([fx; flipud(fx(2:N))]);              % Extend and compute fft
-    fk = fk(1:N+1).*[0.5; ones(N-1,1); 0.5]/N;    % fk contains Chebyshev
-                                                  % coefficients of fx
+    fk = fft( [ fx; flipud( fx(2 : N) ) ] );              % Extend and compute fft
+    fk = fk(1 : N+1) .* [0.5; ones(N-1, 1); 0.5] / N;     % fk contains Chebyshev
+                                                          % coefficients of fx
 end
