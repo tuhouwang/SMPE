a9fec1fbee80e6e9236bb77eec08361c14f2b15d
Tu Houwang <tuhouwang96@163.com>
2021-06-01 10:09:17 +0800

    稍作修改

--- src/ShowSoundField.m
+++ src/ShowSoundField.m
@@ -2,11 +2,10 @@ function ShowSoundField(r,z,tl,tlmin,tlmax,casename)
 
     figure;
     disp('plot the transmission loss field!');
-    pcolor( r./1000, z, tl); hold on;
+    pcolor( r./1000, z, tl); colorbar( 'YDir', 'Reverse' );
     caxis( [tlmin tlmax] ); colormap( flipud(jet) );
-    shading flat; colorbar; view( 0, -90 );
-    xlabel( 'Range (km)')  ; ylabel( 'Depth (m)');
-    colorbar( 'YDir', 'Reverse' );title(casename);
+    shading flat; view( 0, -90 );title(casename);
+    xlabel( 'Range (km)')  ; ylabel( 'Depth (m)');  
     set(gca,'FontSize',20,'FontName','Times New Roman');
 
 end
