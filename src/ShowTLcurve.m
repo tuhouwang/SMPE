38d25463e2e5bd746970ac1e2783b8b8fd74ab6e
Houwang Tu <tuhouwang96@163.com>
2025-10-31 21:36:49 +0800

    A small update for a example!

--- src/ShowTLcurve.m
+++ src/ShowTLcurve.m
@@ -2,7 +2,7 @@ function ShowTLcurve(r,zr,tl_zr)
 
     figure;
     disp('plot the transmission loss curve at zr!');
-    plot(r,tl_zr,'r-','LineWidth',1.5);
+    plot(r,tl_zr,'r-','LineWidth',1);
     set(gca,'YDir','reverse');
     xlabel( 'Range (m)'); ylabel('TL (dB)');
     title(['Depth=',num2str(zr),'m']);
